13fd5baff7379b8768e78747bbe44fcaa7b4ed7a
Jonathan <currie314@gmail.com>
2015-11-06 15:25:52 -0800

    Added the damn files

---
+++ Tuber/src/main/java/com/parse/tuber/CourseBundle.java
@@ -0,0 +1,16 @@
+package com.parse.tuber;
+
+/**
+ * Created by Jonathan on 11/1/15.
+ */
+public class CourseBundle {
+    String id;
+    String name;
+    String grade;
+
+    @Override
+    public String toString() {
+
+        return name + " (Earned Grade: " + grade + ")" ;
+    }
+}
