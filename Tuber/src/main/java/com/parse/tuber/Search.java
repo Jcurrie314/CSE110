e727bb1bc552eea5b05c0590331e7c9034d6a50d
lryu1 <yu.liese@gmail.com>
2015-11-15 23:38:53 -0800

    Added list of engineering departments+classes

--- Tuber/src/main/java/com/parse/tuber/Search.java
+++ Tuber/src/main/java/com/parse/tuber/Search.java
@@ -31,11 +31,39 @@ public class Search extends Activity implements OnItemSelectedListener {
     RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
 
-    String[] cse_numbers = {"Choose a Class to filter", "30", "100", "110"};
-    String[] ece_numbers = {"Choose a Class to filter"};
-    String[] phys_numbers = {};
-    String[] math_numbers = {};
-    String[] chem_numbers = {};
+    String[] cse_numbers = {"Choose a Class to filter", "5A", "7", "8A", "8B", "12", "20",
+            "21", "30", "80", "100", "101", "103", "105", "110", "118", "120", "122", "120",
+            "121", "123", "124", "125", "127", "130", "131", "132A", "132B", "135", "140", "140L",
+            "141", "141L", "143", "144", "145", "148", "150", "151", "152", "153", "160", "164",
+            "165", "166", "167", "168", "169", "170", "181", "182"};
+    String[] ece_numbers = {"Choose a Class to filter", "15", "25", "30", "35", "45", "65", "100",
+            "101", "102", "103", "107", "109", "111", "118", "120", "121", "123", "125A", "125B",
+            "134", "135A", "135B", "136L", "138L", "139", "145", "153", "154", "155", "156", "157",
+            "158", "161", "163", "164", "165", "166", "171", "172", "174", "175", "180"};
+    String[] phys_numbers = {"Choose a Class to filter", "1A", "1B", "1C", "2A", "2B", "2C", "2D",
+            "4A", "4B", "4C", "4D", "4E", "5", "7", "8", "9", "10", "12", "13", "100A", "100B",
+            "100C", "105A", "105B", "110A", "110B", "111", "120", "122", "124", "130A", "130B", "130C"};
+    String[] math_numbers = {"Choose a Class to filter", "10A", "10B", "10C", "11", "15A", "20A", "20B",
+            "20B", "20C", "20D", "20E", "20F", "31AH", "31BH", "31CH", "95", "100A", "100B", "100C",
+            "102", "103A", "103B", "104A", "104C", "109", "110A", "110B", "111A", "111B", "120A",
+            "120B", "121A", "121B", "130A", "130B", "140A", "140C", "142A", "142B", "150A", "150B",
+            "152"};
+    String[] ceng_numbers = {"Choose a Class to filter", "1", "4", "15", "100", "101A", "101B", "101C",
+            "102", "113", "114", "120", "122", "124A", "124B", "134", "157", "176A"};
+    String[] chem_numbers = {"Choose a Class to filter", "4", "6A", "6B", "6C", "7L", "11", "12", "13",
+            "15", "100A", "104", "105A", "105B", "108", "109", "113", "114A", "114B", "114C", "114D",
+            "116", "120A", "120B", "123", "125", "126", "127", "130", "131", "132", "135", "140A",
+            "140B", "1401", "145", "146", "151", "152", "154", "155"};
+    String[] mae_numbers = {"Choose a Class to filter", "02", "05", "07", "08", "20", "92A", "93",
+            "98", "101A", "101B", "101C", "104", "105", "107", "108", "110A", "110B", "113", "117A",
+            "113", "117A", "118", "119", "120", "121", "122", "123", "124", "126A", "126B"};
+    String[] nano_numbers = {"Choose a Class to filter", "1", "4", "15", "100L", "101", "102",
+            "103", "104", "106", "107", "108", "110", "111", "112", "114", "120A", "120B", "141A",
+            "146", "148", "150", "156", "158", "161", "164", "168" };
+    String[] se_numbers = {"Choose a Class to filter", "1", "2", "2L", "3", "9", "101A", "101B", "101C",
+            "101B", "101C", "102", "103", "110A", "110B", "111A-B", "115", "120", "121", "125", "130A-B",
+            "131", "140", "142", "150", "151A", "151B", "152", "154", "160A", "160B", "163", "165", "168"};
+
 
     Spinner department_dropdown, classNumber_dropdown, sortBy_dropdown;
     String dep = "";
@@ -164,6 +192,29 @@ public class Search extends Activity implements OnItemSelectedListener {
                 dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ece_numbers);
 
             } // else if... ECE
+              else if (dep.equals("CE")) {
+                dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ceng_numbers);
+
+            } else if (dep.equals("MAE")) {
+                dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, mae_numbers);
+
+            } else if (dep.equals("SE")) {
+                dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, se_numbers);
+
+            } else if (dep.equals("NANO")) {
+                dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, nano_numbers);
+
+            } else if (dep.equals("MATH")) {
+                dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, math_numbers);
+
+            } else if (dep.equals("PHYS")) {
+                dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, phys_numbers);
+
+            } else if (dep.equals("CHEM")) {
+                dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, chem_numbers);
+
+            }
+
             //TODO: add more options
             dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             classNumber_dropdown.setAdapter(dataAdapter);
